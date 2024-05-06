b7b68292c0d99af7190a4fd03fe2b0fbf11038b2
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-06 22:39:44 +0300

    Ex 3.2

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const buyList = ['Milk', 'Bread', 'Apples']
+// 3.2 ex
+const buyList = ['Milk', 'Bread', 'Apples'];
+console.log(buyList);
+
+buyList.splice(1, 1, 'Bananas', 'Eggs');
+console.log(buyList);
+
+console.log(buyList.pop());
+console.log(buyList);
+
+console.log(buyList.sort());
+
+let findIndex = buyList.indexOf('Milk');
+console.log(findIndex);
+
+buyList.splice(1, 0, 'Carrots', 'Lettuce');
+console.log(buyList);
+
+const buyListTwo = ['Juice', 'Pop'];
+
+let arr1 = buyList.concat(buyListTwo);
+let arr2 = arr1.concat(buyListTwo);
+console.log(arr2);
+
+let findIndex2 = arr2.lastIndexOf('Pop');
+console.log(findIndex2);
+
