a2ec2b81e63d3bbb36424527721e800235824c27
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-07 19:20:27 +0300

    3.5 Ex

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 // 3.2 ex
-const buyList = ['Milk', 'Bread', 'Apples'];
-console.log(buyList);
+// const buyList = ['Milk', 'Bread', 'Apples'];
+// console.log(buyList);
 
-buyList.splice(1, 1, 'Bananas', 'Eggs');
-console.log(buyList);
+// buyList.splice(1, 1, 'Bananas', 'Eggs');
+// console.log(buyList);
 
-console.log(buyList.pop());
-console.log(buyList);
+// console.log(buyList.pop());
+// console.log(buyList);
 
-console.log(buyList.sort());
+// console.log(buyList.sort());
 
-let findIndex = buyList.indexOf('Milk');
-console.log(findIndex);
+// let findIndex = buyList.indexOf('Milk');
+// console.log(findIndex);
 
-buyList.splice(1, 0, 'Carrots', 'Lettuce');
-console.log(buyList);
+// buyList.splice(1, 0, 'Carrots', 'Lettuce');
+// console.log(buyList);
 
-const buyListTwo = ['Juice', 'Pop'];
+// const buyListTwo = ['Juice', 'Pop'];
 
-let arr1 = buyList.concat(buyListTwo);
-let arr2 = arr1.concat(buyListTwo);
-console.log(arr2);
+// let arr1 = buyList.concat(buyListTwo);
+// let arr2 = arr1.concat(buyListTwo);
+// console.log(arr2);
 
-let findIndex2 = arr2.lastIndexOf('Pop');
-console.log(findIndex2);
+// let findIndex2 = arr2.lastIndexOf('Pop');
+// console.log(findIndex2);
 
+// 3.5 ex
+
+const people = {frends:[]};
+
+const frend1 = {name: "Kiirill", lastname: "Blagovestnay", id: 1};
+const frend2 = {name: "Vlad", lastname: "Panamorev", id: 2};
+const frend3 = {name: "Maria", lastname: "Denisova", id: 3};
+
+people.frends.push(frend1, frend2, frend3);
+console.log(people);
