04298f44d5879ac2fae4963c3a26b80211fcfbfb
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-07 19:34:35 +0300

    Проект: Управление Массивом

--- app.js
+++ app.js
@@ -25,13 +25,29 @@
 // let findIndex2 = arr2.lastIndexOf('Pop');
 // console.log(findIndex2);
 
+//----------------------------------------------------------------------------------------------------------------
+
 // 3.5 ex
 
-const people = {frends:[]};
+// const people = {frends:[]};
 
-const frend1 = {name: "Kiirill", lastname: "Blagovestnay", id: 1};
-const frend2 = {name: "Vlad", lastname: "Panamorev", id: 2};
-const frend3 = {name: "Maria", lastname: "Denisova", id: 3};
+// const frend1 = {name: "Kiirill", lastname: "Blagovestnay", id: 1};
+// const frend2 = {name: "Vlad", lastname: "Panamorev", id: 2};
+// const frend3 = {name: "Maria", lastname: "Denisova", id: 3};
 
-people.frends.push(frend1, frend2, frend3);
-console.log(people);
+// people.frends.push(frend1, frend2, frend3);
+// console.log(people);
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Проект: Управление Массивом
+
+const theList = ['Laurence', 'Svekis', true, 35, null, undefined, {test: 'one', score: 55}, ['one', 'two']];
+
+theList.pop(); // Удаление первого элемента массива
+theList.shift(); // Удаление последнего элемента массива
+theList.unshift("First"); // Добавление значения в начало массива
+theList[3] = "helloWorld"; // Присвоение значения 4 элементу
+theList[2] = "MIDDLE"; // Присвоение значения элементу с третьим индексом
+theList.push("LAST"); // Добавление значения в конец массива
+console.log(theList);
